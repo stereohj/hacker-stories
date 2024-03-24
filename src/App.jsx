0b634c5833769a8152f651abd269b4f23e5ebbc8
HJulian Mejia <stereohj-dev@gmail.com>
2024-03-24 15:22:57 -0500

    React Fundamentals s2 - App JS complex obj in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ const welcome = {
   title: 'React'
 };
 
+const array = [0, 1, 2, 3, 4, 5];
+
 function App() {
   return (
     <div>
       <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>{array.map((x) => x * 2 + ' ')}</h1>
 
       <label htmlFor='greeting'>Greeting: </label>
       <input type="text" id="greeting" placeholder='Type a greeting :)'/>
