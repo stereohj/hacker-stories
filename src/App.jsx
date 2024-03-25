83bfd8c1b1e698a3883f67134b8808791b005eaa
HJulian Mejia <stereohj-dev@gmail.com>
2024-03-24 19:09:18 -0500

    React Fundamentals s4 - JS list of objects rendered

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,27 @@ const welcome = {
   title: 'React'
 };
 
+
 const array = [0, 1, 2, 3, 4, 5];
+
+const list = [ 
+  {
+    title: 'React', 
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4, 
+    objectID: 0
+   },
+   {
+    title: 'Redux', 
+    url: 'https://redux.js.org/', 
+    author: 'Dan Abramov, Andrew Clark', 
+    num_comments: 2, 
+    points: 5, 
+    objectID: 1
+  }
+]
 
 function App() {
   return (
@@ -15,6 +35,8 @@ function App() {
 
       <label htmlFor='greeting'>Greeting: </label>
       <input type="text" id="greeting" placeholder='Type a greeting :)'/>
+
+      <h2>{list.map((x) => x.title + " is written by " + x.author + ". ")}</h2>
 
     </div>
   );
